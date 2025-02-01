f68722cbd4bbed67f402cd3647566141545ee5de
rjsd-dule <rumijsd27@gmail.com>
2025-02-01 01:19:42 -0500

    Add new ports

--- ProyectUTP.Domain/obj/Debug/net8.0/ProyectUTP.Domain.AssemblyInfo.cs
+++ ProyectUTP.Domain/obj/Debug/net8.0/ProyectUTP.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectUTP.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f59a8dccbb218a4a06577bd1122e8fc787ae5ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e09a66b9863859b9a2fac060aa671634dbe8cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectUTP.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectUTP.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
